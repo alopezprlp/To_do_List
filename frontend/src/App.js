35dcdf844cf51442b4137acb15c5fa6900e8d7d3
Alex <reybislp@gmail.com>
2022-03-16 18:20:29 -0400

    add todo component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import Nav from "./components/Nav";
+import Todo from "./components/Todo";
 export default function App() {
   return (
     <div className="text-3xl font-bold underline w-full h-screen bg-gray-200">
       <Nav />
+      <Todo/>
     </div>
   );
 }
