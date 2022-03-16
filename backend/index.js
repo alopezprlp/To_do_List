30c45fbaa7c7906a565d52c3bbc66ff916851e2f
Alex <reybislp@gmail.com>
2022-03-16 16:39:55 -0400

    more config

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,24 @@
 const express = require("express");
+const cors = require("cors");
+const parser = require("body-parser");
+
 const app = express();
-const port = 3000;
+
+const router = require("./controllers/todo");
+
+require("dotenv").config();
+
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+app.use(cors());
+app.set("port", process.env.PORT || 3000);
+app.use("/api", router);
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
-app.listen(port, () => {
-  console.log(
-    `Server is successfully running! at http://localhost:${port}`
-  );
+
+app.listen(app.get("port"), () => {
+  console.log(`Server is successfully running! at http://localhost:${app.get("port")}`);
 });
