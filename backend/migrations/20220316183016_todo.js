4b111beecf14e184e02f96f3bc307b38b736e970
Alex <reybislp@gmail.com>
2022-03-16 14:49:57 -0400

    add migration

---
+++ backend/migrations/20220316183016_todo.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("todo", (table) => {
+    table.increments("id").primary();
+    table.string("name", 255).notNullable();
+    table.enum("status", ["DONE", "PENDING"]).defaultTo("PENDING").notNullable();
+    table.timestamps();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("todo");
+};
