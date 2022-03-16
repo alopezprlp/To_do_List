26397bb939b30237db77ec0bb8ab26f63adac5ab
Alex <reybislp@gmail.com>
2022-03-16 18:24:35 -0400

    improve design

--- frontend/src/components/Todo/Item.js
+++ frontend/src/components/Todo/Item.js
@@ -4,8 +4,8 @@ const Item = () => {
   return (
     <div className="w-full p-2 bg-slate-200">
       <div className="flex flex-row justify-between items-center">
-        <p className="text-base">Label</p>
-        <div className="flex items-center justify-between space-x-3">
+        <p className="text-base truncate">Label</p>
+        <div className="flex items-center justify-between space-x-3 pl-2">
           <PencilAltIcon className="h-6 w-6 text-green-500 cursor-pointer" />
           <TrashIcon className="h-6 w-6 text-red-500 cursor-pointer" />
         </div>
